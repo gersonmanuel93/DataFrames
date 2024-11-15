2a700a9281747f47aac42392f1bd33483acc6a7f
Gerson Manuel <78185198+gersonmanuel93@users.noreply.github.com>
2024-11-15 16:57:32 +0100

    AtualizadoDF_1
    1ª Atualização

--- DF_1.py
+++ DF_1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 # Criando um DataFrame a partir de um dicionário
 dados = {
-    "Nome": ["Ana", "Bruno", "Carlos"],
-    "Idade": [23, 34, 29],
-    "Cidade": ["São Paulo", "Rio de Janeiro", "Belo Horizonte"]
+    "Nome": ["Ana", "Bruno", "Carlos", "Manuel"],
+    "Idade": [23, 34, 29, 20],
+    "Cidade": ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Luanda"]
 }
 
 df = pd.DataFrame(dados)
